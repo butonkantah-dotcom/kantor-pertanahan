c93639da0abd4727d32b79f00b89a4cb979bda25
kantahkab_buton <butonkantah@gmail.com>
2025-09-03 12:06:55 +0800

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,22 @@
+/* @type {import('tailwindcss').Config} */
+/*module.exports = {
+//  content: [
+ //   "./pages/**/*.{js,ts,jsx,tsx}",   // scan semua file di pages
+ //   "./components/**/*.{js,ts,jsx,tsx}", // kalau kamu punya folder components
+ //   "./app/**/*.{js,ts,jsx,tsx}", // kalau nanti pakai App Router (Next.js 13+)
+//  ],
+ // theme: {
+//    extend: {},
+ // },
+//  plugins: [],
+//};*/
+
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",   // scan semua file di pages
-    "./components/**/*.{js,ts,jsx,tsx}", // kalau kamu punya folder components
-    "./app/**/*.{js,ts,jsx,tsx}", // kalau nanti pakai App Router (Next.js 13+)
-  ],
+export default {
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
