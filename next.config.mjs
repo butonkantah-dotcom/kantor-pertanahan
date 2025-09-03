91c89384889b3d8fc1e22d007f40c316b400e968
kantahkab_buton <butonkantah@gmail.com>
2025-09-03 11:11:08 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 };
+const path = require("path");
+/** @type {import('next').NextConfig} */
+module.exports = {
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname), // kunci root ke folder proyek
+  },
+};
 
 export default nextConfig;
